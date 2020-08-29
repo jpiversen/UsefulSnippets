2c5e9734fd509191ae9fa16baafda3f7725ace8b
jpiversen <jan.p.iversen@gmail.com>
2020-08-29 16:33:51 +0200

    Add supporting functions

--- R/add_snippets.R
+++ R/add_snippets.R
@@ -3,8 +3,8 @@
 #' Add useful snippets
 #'
 #' This function creates or edits an r.snippets file with custom snippets. After
-#' running this function you will have functions for creating a conection object
-#' for databses and for setting common options.
+#' running this function you will have functions for creating a connection
+#' object for databases and for setting common options.
 #'
 #'
 #' @return Nothing. Creates or modifies a file with custom snippets.
@@ -15,22 +15,11 @@
 #' if (FALSE) add_snippets()
 add_snippets <- function() {
 
-  # Logic stolen from usethis::edit_rstudio_snippets()
-  # https://github.com/cstepper/usethis/blob/master/R/edit.R
-  if (rstudioapi::versionInfo()$version < "1.3") {
-    path <- fs::path_home_r(".R", "snippets", fs::path_ext_set("r", "snippets"))
-  } else {
-    if (.Platform$OS.type == "windows") {
-      path <- fs::path_home_r("AppData/Roaming/Rstudio", "snippets", fs::path_ext_set("r", "snippets"))
-    } else {
-      path <- fs::path_home_r(".config/rstudio", "snippets", fs::path_ext_set("r", "snippets"))
-    }
-  }
-
+  path <- find_snippet_path()
 
   the_snippet <- '
 
-# The snippets below are created with <package name>::add_snippets()
+# The snippets below are created with UsefulSnippets::add_snippets()
 
 snippet snippet_options
 \t# Set options
@@ -59,18 +48,60 @@ snippet snippet_connection
 \t\ttrusted = TRUE
 \t)
 
-# End of snippets created by <package name>::add_snippets()
+# End of snippets created by UsefulSnippets::add_snippets()
 
 '
 
   cat(the_snippet, file = path, append = TRUE)
 
+  file.edit(find_snippet_path())
+
 }
 
 
 
+# Supporting functions ----------------------------------------------------
+
+#' Find path to custom snippet file
+#'
+#' @return Returns the path to the custom snippet file as a string
+#' @export
+#'
+#' @examples
+#' find_snippet_path()
+#'
+find_snippet_path <- function() {
+
+  # Logic stolen from usethis::edit_rstudio_snippets()
+  # https://github.com/cstepper/usethis/blob/master/R/edit.R
+  if (rstudioapi::versionInfo()$version < "1.3") {
+    fs::path_home_r(".R", "snippets", fs::path_ext_set("r", "snippets"))
+  } else {
+    if (.Platform$OS.type == "windows") {
+      fs::path_home_r("AppData/Roaming/Rstudio", "snippets", fs::path_ext_set("r", "snippets"))
+    } else {
+      fs::path_home_r(".config/rstudio", "snippets", fs::path_ext_set("r", "snippets"))
+    }
+  }
+
+}
 
 
+#' Check if custom snippet file already exists
+#'
+#' @return Boolean, TRUE if a custom snipept file exists, else FALSE
+#' @export
+#'
+#' @examples
+#' custom_snippet_file_exsists()
+custom_snippet_file_exsists <- function() {
+
+  as.logical(
+    fs::file_exists(
+      find_snippet_path()
+    )
+  )
+}
 
 
 
